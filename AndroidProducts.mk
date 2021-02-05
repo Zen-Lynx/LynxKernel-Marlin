93c81b3f77c01d8f56ea70118922f577d326f5a4
Lynx <zenial.lynx@gmail.com>
2021-02-05 14:11:19 -0400

    Update LynxOS v0.1.12 ALPHA_DEV

---
+++ AndroidProducts.mk
@@ -0,0 +1,9 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/statix_marlin.mk \
+	$(LOCAL_DIR)/statix_sailfish.mk
+
+COMMON_LUNCH_CHOICES := \
+	statix_marlin-userdebug \
+	statix_sailfish-userdebug \
+        statix_marlin-eng \
+        statix_sailfish-eng
---
+++ statix_sailfish.mk
@@ -0,0 +1,47 @@
+#
+# Copyright 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from StatiX vendor
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
+
+# Inherit device specific makefiles
+$(call inherit-product, device/google/marlin/device-sailfish.mk)
+$(call inherit-product, vendor/google/sailfish/sailfish-vendor.mk)
+
+# Device specific properties
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := statix_sailfish
+PRODUCT_DEVICE := sailfish
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel
+PRODUCT_MANUFACTURER := Google
+
+# GMS client ID
+PRODUCT_GMS_CLIENTID_BASE := android-google
+
+# Device props
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=marlin \
+    BUILD_FINGERPRINT=google/taimen/taimen:10/QQ3A.200605.001/6392402:user/release-keys
